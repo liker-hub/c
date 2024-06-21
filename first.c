047921d5fc89947a5a64dbc8d58d1a306e42ae61
liker-hub <dev506792@gmail.com>
2024-06-21 14:45:19 +0400

    first commit

---
+++ first.c
@@ -0,0 +1,38 @@
+#include<stdio.h> //header file
+int main(){  //keyword
+    // datatype- what type of data
+
+    // int-1,2,3,5
+    // float-1.4,2.9888888 6digits
+    // double-2.566778999999 15digits
+    // char-'',""
+    // 'h',"hello hgh hgjh"/
+
+    // variable-store a data
+
+// syntax- datatype varible-name=value
+    int arc=10;
+    float b=155555;
+    char c='a';
+    char s[]="qwertyuioopasdfghjjkl ";
+    
+// format specifier
+// "%d"   int
+// "%f"  float
+// lf double 
+// "%c" single character
+// "%s"   word or sentencs
+ printf ("%c\n",c);
+
+  printf ("%d\n",arc);
+  
+  printf("%f\n",b);
+  
+  printf ("%s\n",s );
+
+
+    return 0;
+
+    
+}
+
